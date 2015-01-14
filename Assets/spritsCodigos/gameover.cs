132487a2fc997e894e5b06559bacdd1cc50ad1f9
Jaianderson Santos <jaianderson511@gmail.com>
2015-01-14 18:51:36 -0200

    Commit001

---
+++ Assets/spritsCodigos/gameover.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class gameover : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButtonDown ("fire1")) {
+
+			Application.LoadLevel("movimento");
+				}
+	
+	}
+}
